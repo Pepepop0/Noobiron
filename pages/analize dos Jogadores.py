8989b11f9db746dc717dabef1f0e044b4ca23c27
Pedro Antunes <pedro.antunes.ha@gmail.com>
2024-03-18 19:02:19 -0300

    Começando do zero

--- pages/analize dos Jogadores.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from modules.data_loader import load_data
-
-df = load_data("dataset\iron_players.csv")
-
-st.markdown(''' ## Análise dos jogadores ''')
-if st.checkbox("Mostrar Estatísticas Descritivas"):
-    st.write(df.describe())
