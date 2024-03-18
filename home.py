6f1bc9016c5a25685096e584b6642ce523450764
Pedro Antunes <pedro.antunes.ha@gmail.com>
2024-03-18 20:10:15 -0300

    Restart project

---
+++ home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title = "NoobIron",
+    layout = "wide",
+    menu_items = {
+        'About': ''' Dashboard feito para a noobada, possui o selo Pepe popo de qualidade duvidosa
+        - Em caso de derrota um zero deverá ser consultado
+        '''
+    }
+    )
+
+st.write("Tudo funcionando!")
