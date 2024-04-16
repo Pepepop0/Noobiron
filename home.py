f2790f2821e7833d038259b062d9a124e7750c88
Pedro Antunes <pedro.antunes.ha@gmail.com>
2024-04-16 14:29:16 -0300

    Testing onlineDB connection

--- home.py
+++ home.py
@@ -1,11 +1,25 @@
 import streamlit as st
+from players_CRUD import playersCRUD
 
 # Initialize connection.
 conn = st.connection('mysql', type='sql')
+database = playersCRUD()
 
 # Perform query.
 df = conn.query('SELECT * from players_info;', ttl=600)
 
 # Print results.
-for row in df.itertuples():
-    st.dataframe(df)
+st.write('get_all_players():')
+st.write(database.get_all_players())
+
+st.write('get_all_data:')
+st.write(database.get_all_data())
+
+st.write('get_players_names():')
+st.write(database.get_players_names())
+
+st.write('get_players_IDs():')
+st.write(database.get_players_IDs())
+
+st.write('get_players_info():')
+st.write(database.get_players_info())
