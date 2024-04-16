5e9da775bfea1bde92e3e7a9d5634c1c88719f35
Pedro Antunes <pedro.antunes.ha@gmail.com>
2024-04-16 20:51:59 -0300

    image dbg tool

---
+++ pages/3_Dbg_images.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.image('ph_player_icon.png')
+
+player_ID = st.text_input("ID:", value=1001)
+st.image(f'assets\profle_pics\{player_ID}_pfp.png')
+
+if st.button('reload'):
+    st.rerun()
