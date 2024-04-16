42142f95df6c945fa8c7f86582ca4b9b7ccb2c54
Pedro Antunes <pedro.antunes.ha@gmail.com>
2024-04-16 20:57:56 -0300

    test 2

--- pages/3_Dbg_images.py
+++ pages/3_Dbg_images.py
@@ -1,9 +1,7 @@
 import streamlit as st
 
 st.image('ph_player_icon.png')
-
-player_ID = st.text_input("ID:", value=1001)
-st.image(f'assets\profle_pics\ph_player_icon.png')
+st.image(image=f'pfp_test\ph_player_icon.png')
 
 if st.button('reload'):
     st.rerun()
