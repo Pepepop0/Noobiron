51e789b4ee31d83263157a2c5e2123a85cd45280
Pedro Antunes <pedro.antunes.ha@gmail.com>
2024-04-16 21:06:22 -0300

    test 3

--- pages/3_Dbg_images.py
+++ pages/3_Dbg_images.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os
 
 current_directory = os.getcwd()
-image_path = os.path.join(current_directory, 'assets', 'profile_pics', '1001_pfp.png')
+image_path = os.path.join(current_directory, 'profile_pics', '1001_pfp.png')
 
 st.image('ph_player_icon.png')
 st.image(image=image_path)
