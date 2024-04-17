663d145d0e076d151ece8856d396d1294413331e
Pedro Antunes <pedro.antunes.ha@gmail.com>
2024-04-16 21:02:15 -0300

    img test

--- pages/3_Dbg_images.py
+++ pages/3_Dbg_images.py
@@ -2,7 +2,7 @@ import streamlit as st
 import os
 
 current_directory = os.getcwd()
-image_path = os.path.join(current_directory, 'pfp_test', 'ph_player_icon.png')
+image_path = os.path.join(current_directory, 'assets\profle_pics\1001_pfp.png')
 
 st.image('ph_player_icon.png')
 st.image(image=image_path)
