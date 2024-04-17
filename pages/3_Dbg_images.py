f3748daa9179f237ffe948d4242bc5adf66db260
Pedro Antunes <pedro.antunes.ha@gmail.com>
2024-04-16 21:01:00 -0300

    testing fix

--- pages/3_Dbg_images.py
+++ pages/3_Dbg_images.py
@@ -1,7 +1,11 @@
 import streamlit as st
+import os
+
+current_directory = os.getcwd()
+image_path = os.path.join(current_directory, 'pfp_test', 'ph_player_icon.png')
 
 st.image('ph_player_icon.png')
-st.image(image=f'pfp_test\ph_player_icon.png')
+st.image(image=image_path)
 
 if st.button('reload'):
     st.rerun()
