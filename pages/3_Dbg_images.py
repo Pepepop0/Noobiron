11dc5367d793921f3fdbbc6795935a8c7f1e4700
Pedro Antunes <pedro.antunes.ha@gmail.com>
2024-04-16 21:10:16 -0300

    test new folder

--- pages/3_Dbg_images.py
+++ pages/3_Dbg_images.py
@@ -3,10 +3,10 @@ import os
 
 current_directory = os.getcwd()
 image_path = os.path.join(current_directory, 'profile_pics', 'ph_1001_pfp.png')
-image_path = os.path.join(current_directory, 'profile_pics', 'ph_player_icon.png')
+image_path2 = os.path.join(current_directory, 'profile_pics', 'ph_player_icon.png')
 
 st.image('ph_player_icon.png')
 st.image(image=image_path)
-
+st.image(image=image_path2)
 if st.button('reload'):
     st.rerun()
