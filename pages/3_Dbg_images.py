80f3952c4ac29ad12fb94e5fc663ceed6837f547
Pedro Antunes <pedro.antunes.ha@gmail.com>
2024-04-16 21:08:58 -0300

    typo

--- pages/3_Dbg_images.py
+++ pages/3_Dbg_images.py
@@ -2,7 +2,8 @@ import streamlit as st
 import os
 
 current_directory = os.getcwd()
-image_path = os.path.join(current_directory, 'profile_pics', '1001_pfp.png')
+image_path = os.path.join(current_directory, 'profile_pics', 'ph_1001_pfp.png')
+image_path = os.path.join(current_directory, 'profile_pics', 'ph_player_icon.png')
 
 st.image('ph_player_icon.png')
 st.image(image=image_path)
